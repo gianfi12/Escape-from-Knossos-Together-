5af4b482fd39e1f38cfbf9000ed55b378eb5e1b7
gianfi12 <10587213@polimi.it>
2020-11-11 10:31:26 +0100

    Update RoomMaze file

---
+++ Development/EFK/Assets/Scripts/ObjectScript/Door2Script.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Door2Script : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).GetComponent<SingleDoor>().Close();
+            }
+
+            transform.GetComponent<Collider2D>().isTrigger = false;
+        }
+    }
+}
