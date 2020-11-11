13df298dd50cd8763d8f69f5a9397ad81f99ade0
rcrsnt <riccardo.santambrogio@mail.polimi.it>
2020-11-11 10:50:38 +0100

    include missing files

---
+++ Development/EFK/Assets/Scripts/ObjectScript/SingleDoor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleDoor : MonoBehaviour
+{
+    [SerializeField] private float rotation;
+    public void Close()
+    {
+        transform.rotation *= Quaternion.Euler(0,0,rotation);
+    }
+}
