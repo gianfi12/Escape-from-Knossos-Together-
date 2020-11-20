2a3876c6a189ead95969121675090c269dcfd8d6
gianfi12 <10587213@polimi.it>
2020-11-20 16:48:09 +0100

    Updated door

---
+++ Development/EFK/Assets/Scripts/ObjectScript/Door.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    // Is the direction in which the player has to traverse the door in order for it to become closed
+    [SerializeField] private Direction closingDirection;
+
+    private void Awake()
+    {
+        foreach (Door2Script door2Script in transform.GetComponentsInChildren<Door2Script>())
+        {
+            door2Script.Direction = closingDirection;
+        }
+    }
+}
