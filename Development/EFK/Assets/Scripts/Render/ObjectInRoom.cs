d06b25f52c25561497e170fc32b54a67a6f5df56
gianfi12 <10587213@polimi.it>
2020-11-20 22:48:14 +0100

    Updated object managament

---
+++ Development/EFK/Assets/Scripts/Render/ObjectInRoom.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ObjectInRoom
+{
+    private Vector3Int _coordinates;
+    private Transform _transform;
+
+    public ObjectInRoom(Vector3Int coordinates, Transform gameObject)
+    {
+        _coordinates = coordinates;
+        _transform = gameObject;
+    }
+
+    public Vector3Int Coordinates
+    {
+        get => _coordinates;
+        set => _coordinates = value;
+    }
+
+    public Transform ObjectTransform => _transform;
+}
