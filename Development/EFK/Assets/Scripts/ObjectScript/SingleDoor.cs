58feb4bdcdca0510ae287111f724886119e8e8f8
rcrsnt <riccardo.santambrogio@mail.polimi.it>
2020-11-27 18:41:28 +0100

    fix door bug

--- Development/EFK/Assets/Scripts/ObjectScript/SingleDoor.cs
+++ Development/EFK/Assets/Scripts/ObjectScript/SingleDoor.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SingleDoor : MonoBehaviour {
     [SerializeField] private float rotation;
-    private bool isOpen;
+    private bool isOpen=true;
 
     public void Close()
     {
