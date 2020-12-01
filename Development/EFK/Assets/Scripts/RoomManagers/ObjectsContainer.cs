dc39ba9b506b60d986e38d7283f3929bee1cb1fa
gianfi12 <10587213@polimi.it>
2020-12-01 19:39:01 +0100

    Update

---
+++ Development/EFK/Assets/Scripts/RoomManagers/ObjectsContainer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsContainer : MonoBehaviour
+{
+    private int seed;
+    public int Seed => seed;
+
+    public void SetSeed(int seed) {
+        this.seed = seed;
+    }
+}
