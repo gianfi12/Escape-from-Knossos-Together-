8c28656b7858771253cda7631644098eeafab8bf
gianfi12 <10587213@polimi.it>
2020-12-03 19:34:46 +0100

    Commit

---
+++ Development/EFK/Assets/Scripts/EventManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public sealed class EventManager:MonoBehaviour
+{
+    private static readonly object padlock = new object();  
+    private static EventManager instance = null;
+    static Dictionary<EventType, UnityEvent> _eventDictionary = new Dictionary<EventType, UnityEvent>();
+
+    EventManager() 
+    {
+    }  
+    
+    public static void StartListening (EventType eventName, UnityAction listener)
+    {
+        UnityEvent thisEvent = null;
+        if (_eventDictionary.TryGetValue (eventName, out thisEvent))
+        {
+            thisEvent.AddListener (listener);
+        } 
+        else
+        {
+            thisEvent = new UnityEvent ();
+            thisEvent.AddListener (listener);
+            _eventDictionary.Add (eventName, thisEvent);
+        }
+    }
+    
+    public static void StopListening (EventType eventName, UnityAction listener)
+    {
+        UnityEvent thisEvent = null;
+        if (_eventDictionary.TryGetValue (eventName, out thisEvent))
+        {
+            thisEvent.RemoveListener (listener);
+        }
+    }
+
+    public static void TriggerEvent (EventType eventName)
+    {
+        UnityEvent thisEvent = null;
+        if (_eventDictionary.TryGetValue (eventName, out thisEvent))
+        {
+            thisEvent.Invoke ();
+        }
+    }
+    
+    
+    
+    public static EventManager Instance  
+    {  
+        get  
+        {  
+            lock (padlock)  
+            {  
+                if (instance == null)  
+                {  
+                    instance = new EventManager();  
+                }  
+                return instance;  
+            }  
+        }  
+    }
+    
+    
+
+}
