5891860c7cd9f8097a5d09da3ab4f270bf78dcfc
scianna <andrea.sciannamea@gmail.com>
2020-12-21 20:01:45 +0100

    Add Audio Manager

---
+++ Development/EFK/Assets/Scripts/Sound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    [SerializeField] private string name;
+    [SerializeField] private AudioClip clip;
+    [Range(0f,1f)]
+    [SerializeField] private float volume;
+    [Range(.1f,3f)]
+    [SerializeField] private float pitch = 1;
+    [SerializeField] private bool loop;
+    private AudioSource source;
+    public string Name => name;
+    public void SetUpSound(AudioSource audioSource)
+    {
+        source = audioSource;
+        source.clip = clip;
+        source.volume = volume;
+        source.pitch = pitch;
+        source.loop = loop;
+    }
+
+    public void Play()
+    {
+        source.Play();
+    }
+}
