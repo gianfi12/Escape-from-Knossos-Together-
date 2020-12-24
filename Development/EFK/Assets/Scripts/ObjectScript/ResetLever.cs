4d2fb47b83e3f03bf57ed33931ce057c9b1da8b5
gianfi12 <10587213@polimi.it>
2020-12-24 17:49:59 +0100

    Added maze reset lever

---
+++ Development/EFK/Assets/Scripts/ObjectScript/ResetLever.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+
+public class ResetLever: InteractableObject
+{
+    public EntrancePanel EntrancePanel;
+    public override void Interact(GameObject player)
+    {
+        EntrancePanel.ResetButtons();
+    }
+}
