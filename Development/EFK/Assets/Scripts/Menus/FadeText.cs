528952034f442e4a91b13c3aceadfd355e568f1a
scianna <andrea.sciannamea@gmail.com>
2021-01-02 17:37:49 +0100

    Add fade text

---
+++ Development/EFK/Assets/Scripts/Menus/FadeText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeText : ActivatableObject
+{
+    public override void ActivateObject()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public override void DeactivateObject()
+    {
+        gameObject.SetActive(false);
+    }
+}
